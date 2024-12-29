a38279db4be64c462744c53f4c531dcd6db252d0
segunojo1 <olusegunkd2017@gmail.com>
2024-12-29 03:55:09 +0100

    feat: google auth step one

--- src/modules/auth/login/SocialLogin.tsx
+++ src/modules/auth/login/SocialLogin.tsx
@@ -1,12 +1,17 @@
+'use client'
 import { Button } from '@/components/ui/button'
+import { signIn, useSession } from 'next-auth/react'
 import Image from 'next/image'
 import React from 'react'
 
 const SocialLogin = () => {
+  const { data: session } = useSession();
+console.log(session);
+
   return (
     <div className="flex flex-col justify-between gap-4 mt-4">
     <p className='text-desktop-highlight font-bold mx-auto'>Or</p>
-    <Button className="mx-auto p-[10px] flex-[.7] border-2 md:w-[350px] w-full border-primary-bg-800 text-primarygtext rounded-[30px] flex items-center gap-[10px] text-desktop-highlight font-bold">
+    <Button className="mx-auto p-[10px] flex-[.7] border-2 md:w-[350px] w-full border-primary-bg-800 text-primarygtext rounded-[30px] flex items-center gap-[10px] text-desktop-highlight font-bold" onClick={() => signIn("google")}>
       <Image src="/assets/google.png" width={32} height={32} alt='google' />
       Sign in with Google
     </Button>
