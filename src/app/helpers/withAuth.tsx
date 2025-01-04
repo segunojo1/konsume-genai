b2bddbd3e6111603a01411e2adfd34c9a42b1f29
segunojo1 <olusegunkd2017@gmail.com>
2025-01-04 02:30:47 +0100

    feat: scanner

--- src/app/helpers/withAuth.tsx
+++ src/app/helpers/withAuth.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
-import { useRouter } from "next/router";
 import isAuthenticated from "./isAuthenticated";
 import Cookies from "js-cookie";
+import { useRouter } from "next/navigation";
 
 // this would change later on once backend has the authentication
 // working.
