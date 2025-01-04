f733fbebed7e90cb963ec81a84fb28c7ca254868
segunojo1 <olusegunkd2017@gmail.com>
2025-01-04 13:11:36 +0100

    next auth fixes

--- auth.ts
+++ auth.ts
@@ -1,7 +1,27 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
 
-import NextAuth from "next-auth"
-import Google from "next-auth/providers/google"
- 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [Google],
-})
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, account }) {
+      // Persist the access and refresh tokens to the JWT
+      if (account) {
+        token.accessToken = account.access_token;
+        token.refreshToken = account.refresh_token;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      // Attach the access and refresh tokens to the session object
+      session.accessToken = token.accessToken;
+      session.refreshToken = token.refreshToken;
+      return session;
+    },
+  },
+});
---
+++ src/@types/next-auth.d.ts
@@ -0,0 +1,14 @@
+// types/next-auth.d.ts
+import NextAuth from "next-auth";
+
+declare module "next-auth" {
+  interface Session {
+    accessToken?: string;
+    refreshToken?: string;
+  }
+
+  interface JWT {
+    accessToken?: string;
+    refreshToken?: string;
+  }
+}
