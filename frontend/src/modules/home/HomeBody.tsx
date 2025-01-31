5596b1e759b495f001b214655bba819ef51d26a9
segunojo1 <olusegunkd2017@gmail.com>
2025-01-31 17:41:02 +0100

    remove img

--- frontend/src/modules/home/HomeBody.tsx
+++ frontend/src/modules/home/HomeBody.tsx
@@ -26,17 +26,17 @@ const HomeBody = () => {
     <div className="flex  flex-col font-satoshi  " ref={scrollRef}>
       <main className="flex  flex-col " >
         <Hero />
-        <motion.div ref={panRef} className=" hidden -top-[40%] md:block  z-0" style={{ position: "sticky", top: 0 }}>
+        {/* <motion.div ref={panRef} className=" hidden -top-[40%] md:block  z-0" style={{ position: "sticky", top: 0 }}>
           <motion.div className="absolute  right-0 -top-[55vh]" style={{opacity: useTransform(scrollYProgress, [0.5, 0.8], [1, 0]), top: useTransform(scrollYProgress, [0, 0.4], [-900, 60]), right: useTransform(scrollYProgress, [0.2, 0.7], [0, 400]), rotate: useTransform(scrollYProgress, [0.2, 0.7], [0, 60]) }}>
             <Image
               src={pan}
               alt="pan"
-              className=""
+              className="z-[999999999999999] relative"
               width={858}
               height={869}
             />
           </motion.div>
-        </motion.div>
+        </motion.div> */}
         <motion.div ref={panRef} className=" block -top-[40%] md:hidden  z-0" style={{ position: "sticky", top: 0 }}>
           <motion.div className="absolute  right-0 top-[80vh]" style={{opacity: useTransform(scrollYProgress, [0.5, 0.8], [1, 0]), top: useTransform(scrollYProgress, [0, 0.4], [-900, 800]), right: useTransform(scrollYProgress, [0.2, 0.7], [0, 100]), rotate: useTransform(scrollYProgress, [0.2, 0.7], [0, 60]) }}>
             <Image
